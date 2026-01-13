a09e5822578abcffdbeb6c8b1c23193e8557d515
rahul-singhania223 <rahulsinghania406@gmail.com>
2026-01-13 11:54:54 +0530

    updated api config in store

--- store/config/api.config.ts
+++ store/config/api.config.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+let serverUrl =
+  process.env.NEXT_PUBLIC_PRODUCTION_SERVER_URL || "http://localhost:5000";
+
 const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_PRODUCTION_SERVER_URL ||
-    "http://localhost:5000" + "/api/v1",
+  baseURL: serverUrl + "/api/v1",
   timeout: 10000,
   withCredentials: true,
   headers: {
